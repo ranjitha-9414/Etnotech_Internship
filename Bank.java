d77025ee285db9ae52780e1cf3f11ef2bd8dc506
ranjitha-9414 <cranjitha82@gmail.com>
2026-02-12 11:07:55 +0530

    Day3

--- Bank.java
+++ Bank.java
@@ -1,10 +1,38 @@
+import java.util.*;
 public class Bank {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
         BankAccount account1=new BankAccount(1000);
         System.out.println("Intial Balance: "+ account1.getBalance());
-        account1.deposit(2000);
+        System.out.println("Enter amount to deposit :");
+        
+        double deposit=sc.nextDouble();
+        account1.deposit(deposit);
         System.out.println("Balance after deposit: "+ account1.getBalance());
-        account1.withdrawl(500);
-        System.out.println("Balance after withdrawal: "+ account1.getBalance());
+        while (true) {
+            System.out.println("Do you want to withdrawl amount Y/N:");
+            String choice=sc.next().toUpperCase();
+            switch (choice) {
+                case "Y":
+                    System.out.println("Enter amount to withdrawl:");
+                    double withdrawl=sc.nextDouble();
+                    account1.withdrawl(withdrawl);
+                    System.out.println("Balance after withdrawal: "+ account1.getBalance());
+                    break;
+                case "N":
+                    System.out.println("Thank you for using our services.");
+                    sc.close();
+                    return;
+                    
+                default:
+                    System.out.println("Invalid Input");
+                
+        }
     }
+    
+       
+
+    }
+
+    
 }
